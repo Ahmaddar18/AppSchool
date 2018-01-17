cf21fd8e91063ca0b44f58cb9a82509777bc76b5
Ahmad Ijaz <ahmad@ahmad.local>
2018-01-17 21:10:01 +0100

    Basic Skelton is ready

---
+++ AppSchool/CalenderVC.swift
@@ -0,0 +1,35 @@
+//
+//  CalenderVC.swift
+//  AppSchool
+//
+//  Created by Ahmad on 1/16/18.
+//  Copyright © 2018 Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class CalenderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.title = "Calender"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AppSchool/ViewController.swift
+++ AppSchool/ViewController.swift
@@ -4,22 +4,173 @@
 //
 //  Created by Ahmad on 1/11/18.
 //  Copyright © 2018 Ahmad. All rights reserved.
-//
 
+import Foundation
 import UIKit
+var sidebarView: SidebarView!
+var blackScreen: UIView!
+class ViewController: UIViewController, UITextFieldDelegate {
+    struct User : Codable {
+        let RESPONSE: String
+        let TOKEN: String
+        let MENSAGEMERRO: String
+        let MENSAGEMSUCESSO: String
+        let STATUS: String
+        let TYPE: String
+        let NOMEUSER: String?
+        let EMAILUSER: String?
+    }
+    
+    @IBOutlet weak var EmailTextView: UITextField!
+    @IBOutlet weak var SenhaTextField: UITextField!
+    
+    var loadIndicator: UIView = UIView()
+    
+    var window: UIWindow?
+    
+    @IBAction func doLogin(_ sender: Any) {
+        print("doLogin")
+        
+        let email = EmailTextView.text
+        let senha = SenhaTextField.text
+        
+        if email == "" {
+            UIHelper.showAlertController(uiController: self, message: "Por favor, preencha o e-mail")
+            return
+        } else if senha == "" {
+            UIHelper.showAlertController(uiController: self, message: "Por favor, preencha a senha")
+            return
+        } else {
+            self.loadIndicator = UIHelper.activityIndicator(uiController: self, title: "Carregando")
+            doLoginRequest(email: email!, senha: senha!)
+        }
+    }
+    
+    func doLoginRequest(email: String, senha: String){
+        let postString = """
+        {
+        \"setEmail\"    :"\(email)",
+        \"setSenha\"    :"#Hash('\(senha)', 'MD5')#",
+        \"setSistema\"    :"Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46",
+        \"setUdid\"    :"93C86FG587487CC4876S454GHT13"
+        }
+        """
+        
+        var request = URLRequest(url: URL(string: "http://52.10.244.229:8888/rest/wsapimob/autenticaracesso")!)
+        request.httpMethod = "POST"
+        request.addValue("PROD", forHTTPHeaderField: "TAmb")
+        
+        print(postString)
+        
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                
+                UIHelper.showAlertController(uiController: self, message: "Não foi possível conectar ao servidor", title: "Erro")
+                return
+            }
+            
+            let httpStatus = response as? HTTPURLResponse
+            print("httpStatus = \(String(describing: httpStatus))")
+            if httpStatus?.statusCode == 200 {
+                let responseString = String(data: data, encoding: .utf8)
+                print("responseString = \(String(describing: responseString))")
+                
+                let retorno = responseString?.data(using: .utf8)!
+                
+                let decoder = JSONDecoder()
+                let user = try! decoder.decode(User.self, from: retorno!)
+                
+                if user.RESPONSE == "200" {
+                    DispatchQueue.main.async {
+                        UIHelper.stopsIndicator(view: self.loadIndicator)
+                        print("ok")
+                                self.window = UIWindow(frame:UIScreen.main.bounds)
+//                                if let window = self.window{
+//                                    window.backgroundColor = UIColor.white
+//                                    let nav = UINavigationController()
+//                                    let mainView = HomeViewController()
+//                                    nav.viewControllers = [mainView]
+//                                    window.rootViewController = nav
+//                                    window.makeKeyAndVisible()
+//                                }
+                        
+//        let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController")
+//
+//        self.present(homeViewController!, animated: true, completion: nil)
+//        self.window?.makeKeyAndVisible()
+        
+        let viewController: HomeViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
 
-class ViewController: UIViewController {
-
+        self.navigationController?.pushViewController(viewController, animated: false)
+                        
+                        //
+                        //                        self.window?.rootViewController =
+                        
+                        
+                        //                        let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+                        //                        let redViewController = mainStoryBoard.instantiateViewController(withIdentifier: "GradesViewController") as! GradesViewController
+                        //                        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                        //                        appDelegate.window?.rootViewController = redViewController
+                        
+                        
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        UIHelper.showAlertController(uiController: self, message: user.MENSAGEMERRO)
+                        UIHelper.stopsIndicator(view: self.loadIndicator)
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    @IBAction func goToEsqueciSenha(_ sender: Any) {
+        print("doEsqueciSenha")
+        let lostPasswordViewController = self.storyboard?.instantiateViewController(withIdentifier: "forgotPasswordViewController")
+        self.present(lostPasswordViewController!, animated: true, completion: nil)
+    }
+    
+    @IBAction func goToSuporte(_ sender: Any) {
+        print("doSuporte")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+      
+        
+        EmailTextView?.text = "teste@empresa.com"
+        SenhaTextField?.text = "54321"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        SenhaTextField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+       // ScrollView.setContentOffset(CGPoint(x: 0,y: 100), animated: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+     //   ScrollView.setContentOffset(CGPoint(x: 0,y: 0), animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
 }
-
